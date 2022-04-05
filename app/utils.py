7fe0f0d35014a4f59ab130aceb8ff414db906675
jaydeshmukh14 <97574516+jaydeshmukh14@users.noreply.github.com>
2022-04-05 10:23:14 -0400

    utils.py update
    Updated utils file to remove the global scope of unofficial testing

--- app/utils.py
+++ app/utils.py
@@ -9,3 +9,18 @@ def to_usd(my_price):
     Invoke like this: to_usd(9.999). The function will return $9.99
     """
     return '${:,.2f}'.format(my_price)
+
+# unofficial testing:
+# if this code is in the global scope
+# ... of a file we are trying to import from
+# ... it will throw errors
+#price = input("Please choose a price like $4.999999")
+#print(to_usd(float(price)))
+
+# to fix this, we will do this:
+# nesting the code in the main conditional will fix the errors we were being thrown
+if __name__ == "__main__":
+    price = input("Please choose a price like $4.999999")
+    print(to_usd(float(price)))
+
+
